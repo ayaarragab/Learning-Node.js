7f700cd4a0f99cbf35feecf088acad8e693ec0c0
ayaarragab <ayarsk4343@gmail.com>
2024-07-07 19:56:55 +0300

    path problem in db.js solved and the program works!

--- note-taking-app/src/db.js
+++ note-taking-app/src/db.js
@@ -1,6 +1,6 @@
 import exp from 'node:constants';
 import fs from 'node:fs/promises';
-const DB_PATH = new URL('./db.json', import.meta.url).pathname
+const DB_PATH = new URL('../db.json', import.meta.url).pathname
 
 export const getDB = async () => {
     const readDB = await fs.readFile(DB_PATH, 'utf-8'); // 'utf-8' is for encoding human lang
