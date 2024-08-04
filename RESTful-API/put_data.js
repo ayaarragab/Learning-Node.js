ee2f9fda8ea2fb6c4f7bb823e6bbf0044583a5c6
ayaarragab <ayarsk4343@gmail.com>
2024-08-04 11:48:00 +0300

    removed unnecessary files

--- RESTful-API/put_data.js
+++ RESTful-API/put_data.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
+import mongoose from 'mongoose';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import Job from "./models/job.js";
+import User from "./models/user.js";
+import Application from "./models/application.js";
+
+// Get the current file path and directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost/job-board-api', {
@@ -23,11 +31,6 @@ db.once('open', async function () {
     const jobsData = JSON.parse(fs.readFileSync(jobsPath, 'utf-8'));
     const applicationsData = JSON.parse(fs.readFileSync(applicationsPath, 'utf-8'));
     const usersData = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
-
-    // Define Mongoose models
-    const Job = mongoose.model('Job', new mongoose.Schema({}, { strict: false }));
-    const Application = mongoose.model('Application', new mongoose.Schema({}, { strict: false }));
-    const User = mongoose.model('User', new mongoose.Schema({}, { strict: false }));
 
     // Insert data into collections
     await Job.insertMany(jobsData);
