3c9d5c55cd9b2cd378a308edffffbd88b063e41f
ayaarragab <ayarsk4343@gmail.com>
2024-08-11 18:26:49 +0300

    added extra important propertites to user model

--- RESTful-API/models/user.js
+++ RESTful-API/models/user.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 export const UserSchema = new mongoose.Schema({
-    name:{type: String, required: true, unique: true},
+    fullName:{ type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isCEO: {type: Boolean, default: false}
+    status: { type: String, enum: ['employed', 'unemployed', 'student'], required: true },
+    professional_title: { type: String, required: false },
+    company: { type: mongoose.Schema.Types.ObjectId, required: false, ref:'Company' }
 })
+
 const User = mongoose.model('User', UserSchema);
+
 export default User;
