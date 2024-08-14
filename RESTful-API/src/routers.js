bfc5474b2dee2a9a342a19cc6112f3029e500b6c
ayaarragab <ayarsk4343@gmail.com>
2024-08-14 10:20:42 +0300

    recover

--- RESTful-API/src/routers.js
+++ RESTful-API/src/routers.js
@@ -27,24 +27,22 @@ router.get('/alljobs', helpers.handleErrors, helpers.getAllJobs);
 /**
  * Company endpoints
  */
-router.get('/companies', helpers.handleErrors, helpers.getAllCompanies) // ✔
-router.get('/company', helpers.companyValidationsGET, helpers.handleErrors, helpers.getCompanyInfo); // ✔
+router.get('/companies', helpers.handleErrors, helpers.getAllCompanies)
+router.get('/company', helpers.companyValidationsGET, helpers.handleErrors, helpers.getCompanyInfo);
 
-router.post('/company', helpers.companyValidationsPOST, helpers.handleErrors, helpers.isCEO, helpers.createCompany); // ✔
-router.put('/company', helpers.handleErrors, helpers.isCEO, helpers.updateCompanyInfo); // ✔
-router.delete('/company', helpers.handleErrors, helpers.isCEO, helpers.deleteCompany); // ✔
-
-
-router.post('/company/jobs', helpers.isEligible , helpers.jobValidationsPOST, helpers.handleErrors, helpers.createJob); // ✔
-router.put('/company/jobs', helpers.isEligible, helpers.handleErrors, helpers.updateJob); // ✔
-router.delete('/company/jobs', helpers.isEligible, helpers.handleErrors, helpers.jobValidationsGET, helpers.deleteJob); // ✔
-router.get('/company/jobs', helpers.handleErrors, helpers.jobValidationsGET, helpers.getCompanyJobs); // ✔
-router.get('/:companyName/:jobTitle', helpers.handleErrors, helpers.getJob); // ✔
+router.post('/company', helpers.companyValidationsPOST, helpers.handleErrors, helpers.isCEO, helpers.createCompany);
+router.put('/company', helpers.handleErrors, helpers.isCEO, helpers.updateCompanyInfo);
+router.delete('/company', helpers.handleErrors, helpers.isCEO, helpers.deleteCompany);
 
 
-router.get('company/employees', helpers.handleErrors, /*getCompanyEmployees*/);
-router.post('company/employees', helpers.handleErrors, /*addEmployee*/);
-router.put('company/employees', helpers.handleErrors, /*UpdateEmployeeInfo*/);
-router.delete('company/employees', helpers.handleErrors, /*deleteEmployee*/);
+router.post('/company/jobs', helpers.isEligible , helpers.jobValidationsPOST, helpers.handleErrors, helpers.createJob);
+router.put('/company/jobs', helpers.isEligible, helpers.handleErrors, helpers.updateJob);
+router.delete('/company/jobs', helpers.isEligible, helpers.handleErrors, helpers.jobValidationsGET, helpers.deleteJob);
+router.get('/company/jobs', helpers.handleErrors, helpers.jobValidationsGET, helpers.getCompanyJobs);
+router.get('/:companyName/:jobTitle', helpers.handleErrors, helpers.getJob);
+
+
+router.get(':companyName/employees', helpers.handleErrors, /*getCompanyEmployees*/);
+router.delete(':companyName/employees', helpers.handleErrors, /*deleteEmployee*/);
 
 export default router;
--- RESTful-API/src/serverUsingExpress.js
+++ RESTful-API/src/serverUsingExpress.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from 'path';
 import router from "./routers.js";
-import morgan from "morgan"; // useful middleware
+import morgan from "morgan";
 import cors from "cors";
 import {protect} from "./modules/auth.js";
 import * as handlers from "./handlers/user.js";
@@ -13,53 +13,17 @@ app.use(cors());
 
 app.use(morgan('dev'))
 
-app.use(express.json()); // basically it allows client to send us json 
-
-app.use(express.urlencoded({extended: true})); // basically it allows client to send us query string in a object instead of in a string 
-
-/**
- * making my own middleware
- * app.use((req, res, next) => {})
- * same as app.use(express.json()) or app.use(morgan('dev'))
- * but the previous middlewares are bultin in express, if we want to create our own, it will be constructed like this
- * app.use((req, res, next) => {})
- * And this is how every middleware is made in the background
- */
-
-// app.use((req, res, next) => {
-//     req.secret = "secret don't tell anyone";
-//     next();
-// })
-
-/**
- * do you know what is this next() ? it's the next middleware literally !
- * It's the below middleware starts with app.get('/', (req, res) =>
- * everything that is attached by app after this will be able to access req.secret (why?)
- */
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('public/index.html'));
 })
 
-// app.use() allows you to apply global configurations either on a specific path or on the entire app
 
-app.use('/api', protect, router); // not a middleware, concatenated '/api' to be first in all paths strings
-
-/**
- * The following routes have not to be protected
- * as the person who signin or register will not have JWT
- *  */
+app.use('/api', protect, router);
 
 app.post('/register', handlers.createUser);
 app.post('/signin', handlers.signin);
-/**
- * app.get("/todo/:id", mymiddleware1, mymiddleware2, handler);
- * or app.get("/todo/:id", [mymiddleware1, mymiddleware2], handler);
- * How to make your middleware takes arguemnts? by writing a function that returns a function !
- */
-/* const myMiddleWare = (param1, param2) => (req, res, next) => {
-    console.log(`Hello in my myMiddleWare that takes ${param1} ${param2}`);
-}*/
 
 
 export default app;
